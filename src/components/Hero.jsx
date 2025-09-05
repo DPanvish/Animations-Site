a9532d0d2f89b72f5447f65b13a21016986771d9
Panvish Dowripilli <panvishd@gmail.com>
2025-09-06 02:40:53 +0530

    Styling the Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,18 +23,18 @@ const Hero = () => {
   const getVideoSrc = (idx) => `videos/hero-${idx}.mp4`;
 
   return (
-    <div className="relative h-dvh w-screen overflow-x-hidden">
-      <div id="video-frame" className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75">
+    <div className="relative w-screen overflow-x-hidden h-dvh">
+      <div id="video-frame" className="relative z-10 w-screen overflow-hidden rounded-lg h-dvh bg-blue-75">
         <div>
-          <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
-            <div onClick={handleMiniVideoClick} className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100">
+          <div className="absolute z-50 overflow-hidden rounded-lg cursor-pointer mask-clip-path absolute-center size-64">
+            <div onClick={handleMiniVideoClick} className="transition-all duration-500 ease-in origin-center scale-50 opacity-0 hover:scale-100 hover:opacity-100">
               <video
                 ref={nextVideoRef}
                 src={getVideoSrc((currentIdx % totalVideos) + 1)}
                 loop
                 muted
                 id="current-video"
-                className="size-64 origin-center scale-150 object-cover object-center"
+                className="object-cover object-center origin-center scale-150 size-64"
                 onLoadedData={handleVideoLoad}
               />
             </div>
@@ -46,7 +46,7 @@ const Hero = () => {
             loop
             muted
             id="next-video"
-            className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
+            className="absolute z-20 invisible object-cover object-center absolute-center size-64"
             onLoadedData={handleVideoLoad}
           />
 
@@ -55,9 +55,19 @@ const Hero = () => {
             autoPlay
             loop
             muted
-            className="absolute left-0 top-0 size-full object-cover object-contain"
+            className="absolute top-0 left-0 object-contain object-cover size-full"
             onLoadedData={handleVideoLoad}
           />
+        </div>
+
+        <h1 className="absolute z-40 special-font hero-heading bottom-5 right-5 text-blue-75">G<b>a</b>ming</h1>
+
+        <div className="absolute top-0 left-0 z-40 size-full">
+          <div className="px-5 mt-24 sm:10">
+            <h1 className="text-blue-100 special-font hero-heading">redefi<b>n</b>E</h1>
+
+            <p className="mb-5 text-blue-100 max-w-64 font-roboto-regular">Enter the Metagame Layer <br /> Unleash the Play Economy</p>
+          </div>
         </div>
       </div>
     </div>
