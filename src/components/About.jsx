30c1999f150056134a1a6f17f3da5d5f15b96420
Panvish Dowripilli <panvishd@gmail.com>
2025-09-09 01:44:26 +0530

    Animated Title Component Done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import gsap from 'gsap'
 
 import ScrollTrigger from 'gsap/all'
+import AnimatedTitle from './AnimatedTitle';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -40,9 +41,10 @@ const About = () => {
       <div className="relative flex flex-col items-center gap-5 mb-8 mt-36">
         <h2 className="font-general text-sm uppercase md:text-[20px]">Welcome to Zentry</h2>
 
-        <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
-            Disc<b>o</b>ver the world's <br/> l<b>a</b>rgest shared adventure
-        </div>
+        <AnimatedTitle 
+          title="Disc<b>o</b>ver the world's <br/> l<b>a</b>rgest shared adventure"
+          containerClass="mt-5 !text-black text-center"
+        />
 
         <div className="about-subtext">
             <p>The Game of Games begins-your life, now an epic MMORPG</p>
