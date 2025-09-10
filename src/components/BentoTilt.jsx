505b13d6fecf69441c241fcf1faaf5f957e007e3
DOWRIPILLI PANVISH <119529390+DPanvish@users.noreply.github.com>
2025-09-11 02:16:01 +0530

    BentoTilt Complete

--- src/components/BentoTilt.jsx
+++ src/components/BentoTilt.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
-const BentoTilt = () => {
+const BentoTilt = ({children, className=""}) => {
+  const [transformStyle, setTransformStyle] = useState("");
+
+  const itemRef = useRef();
+
+  const handleMouseMove = (e) => {
+    if(!itemRef.current){
+      return;
+    }
+
+    const {left, top, width, height} = itemRef.current.getBoundingClientRect();
+
+    const relativeX = (e.clientX - left) / width;
+    const relativeY = (e.clientY - top) / height;
+
+    const tiltX = (relativeX - 0.5) * 8;
+    const tiltY = (relativeY - 0.5) * -8;
+
+    const newTransform = `perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d(0.98, 0.98, 0.98)`;
+
+    setTransformStyle(newTransform);
+  }
+
+  const handleMouseLeave = () => {
+    setTransformStyle("");
+  }
+
   return (
-    <div>
-      
+    <div 
+      className={className} 
+      ref={itemRef} 
+      onMouseMove={handleMouseMove} 
+      onMouseLeave={handleMouseLeave}
+      style={{transform: transformStyle}}
+    >
+      {children}
     </div>
   )
 }
